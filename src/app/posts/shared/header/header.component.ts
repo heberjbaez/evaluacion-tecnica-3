46cb03683b8c623bea9fcc69753bc46e9925205e
Heber Baez <hbaez@mobydigital.com>
2022-09-19 06:21:46 -0300

    userName visible

--- src/app/posts/shared/header/header.component.ts
+++ src/app/posts/shared/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  userName: string = '';
+  constructor() {}
 
   ngOnInit(): void {
+    this.loadUser();
   }
 
+  loadUser() {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    this.userName = user[0].name;
+  }
 }
